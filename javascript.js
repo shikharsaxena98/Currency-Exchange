0e465c8897b84773ff1fd64ace60f1aebab199e3
h0l0x <30898042+h0l0x@users.noreply.github.com>
2018-08-05 19:41:50 +0530

    Add files via upload
    Changes made to javascript.js to complete XMLhttpRequest

--- javascript.js
+++ javascript.js
@@ -1,11 +1,31 @@
 var xhr = new XMLHttpRequest();
-xhr.open('GET', '/server', true);
 
-var arr = {
-    curr1: document.getElementById('x').value,
-    curr2: document.getElementById('y').value,
-    val1: document.getElementById('a').value,
-    val2: document.getElementById('b').value
-};
+function process() {
+    if(xhr.readyState==0 || xhr.readyState==4){
+        var arr = {
+            curr1: document.getElementById('x').value,
+            curr2: document.getElementById('y').value,
+        };
+        xhr.open('GET', 'https://exchangeratesapi.io/api/latest?base=' + arr.curr1 + '&symbols=' + arr.curr2, true);
+        xhr.onreadystatechange = function() {
+            if (this.readyState == 4 && this.status == 200) {
+                var myArr = JSON.parse(this.responseText);
+                console.log(myArr);
+                if(xhr.status==200){
+                    c = document.getElementById('y').value;
+                    val2 = myArr.rates.c;
+                    console.log(val2);
+                    document.getElementById("b").innerHTML = val2;
+                    setTimeout('process()',1000);
+                }
+            }
+        } 
+        xhr.send(null);
+    }else{
+        setTimeout('process()',1000);
+    }
+  
+}
 
-xhr.send(arr);
+
+
